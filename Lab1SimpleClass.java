a78f7c917c4ad5d7f0af720ecbe40ca64a73a0c4
Khawoat231239 <phattaraphon.c@ku.th>
2018-01-14 09:49:48 +0700

    text

---
+++ Lab1SimpleClass.java
@@ -0,0 +1,12 @@
+// 1.5เริ่มเขยีนภาษาJAVAและทดลองสั่งงาน
+
+public class Lab1SimpleClass
+{
+  public static void main(String[] args)
+  {
+    //System.out.println("18 and 81: " + 24 + 45);
+    // >>> 18 and 81: 2445
+    System.out.println("18 and 81: " + (24 + 45));
+    // >>> 18 and 81: 69
+  }
+}
